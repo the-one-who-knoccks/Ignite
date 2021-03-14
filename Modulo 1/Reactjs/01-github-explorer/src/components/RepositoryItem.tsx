c56ed3ff9d68c65caa22274d83c8936fbe204a91
the-one-who-knoccks <thiagoalves.devp@gmail.com>
2021-03-14 19:23:37 -0300

    Changed jsx files to tsx
    React files

--- Modulo 1/Reactjs/01-github-explorer/src/components/RepositoryItem.tsx
+++ Modulo 1/Reactjs/01-github-explorer/src/components/RepositoryItem.tsx
@@ -1,8 +1,17 @@
-export default function RepositoryItem(props) {
+interface RepositoryItemProps {
+  repository: {
+    name: string;
+    description: string;
+    html_url: string;
+  }
+}
+
+export default function RepositoryItem(props : RepositoryItemProps) {
   return (
     <li>
       <strong>{props.repository.name}</strong>
       <p>{props.repository.description}</p>
+
       <a href={props.repository.html_url}>Acessar repositório</a>
     </li>
   );
--- Modulo 1/Reactjs/01-github-explorer/src/components/RepositoryList.tsx
+++ Modulo 1/Reactjs/01-github-explorer/src/components/RepositoryList.tsx
@@ -4,8 +4,14 @@ import RepositoryItem from "../components/RepositoryItem";
 
 import "../styles/repositories.scss";
 
+interface Repository {
+  name: string;
+  description: string;
+  html_url: string;
+}
+
 export default function RepositoryList() {
-  const [repositories, setRepositories] = useState([]);
+  const [repositories, setRepositories] = useState<Repository[]>([]);
 
   useEffect(() => {
     fetch("https://api.github.com/orgs/rocketseat/repos")
