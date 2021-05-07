b7a1a205da4b51dbf24c0d596066def3e2999903
the-one-who-knoccks <thiagoalves.devp@gmail.com>
2021-05-06 23:26:31 -0300

    :sparkles: Created the TransactionsTable component
    Created transactions component

---
+++ Modulo-02/dtmoney/src/components/TransactionsTable/index.tsx
@@ -0,0 +1,38 @@
+import { Container } from './styles';
+
+export function TransactionsTable() {
+  return (
+    <Container>
+      <table>
+        <thead>
+          <tr>
+            <th>Titulo</th>
+            <th>Valor</th>
+            <th>Categoria</th>
+            <th>Data</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>Desenvolvimento de website</td>
+            <td className="deposit">R$12.000</td>
+            <td>Desenvolvimento</td>
+            <td>06/05/2021</td>
+          </tr>
+          <tr>
+            <td>Aluguel</td>
+            <td className="withdraw">-R$1.100</td>
+            <td>Casa</td>
+            <td>04/05/2021</td>
+          </tr>
+          <tr>
+            <td>Pagamento do mês</td>
+            <td className="deposit">R$3.500</td>
+            <td>Foton Tech</td>
+            <td>06/05/2021</td>
+          </tr>
+        </tbody>
+      </table>
+    </Container>
+  );
+}
