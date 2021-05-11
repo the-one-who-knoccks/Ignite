cbf8ccbf38a3a989ec9933ce63cf72e13c60e760
the-one-who-knoccks <thiagoalves.devp@gmail.com>
2021-05-11 01:05:20 -0300

    :sparkles: Created the Modal component
    Created the modal component

---
+++ Modulo-02/dtmoney/src/components/NewTransactionModal/index.tsx
@@ -0,0 +1,52 @@
+import Modal from 'react-modal';
+import { Container } from './styles';
+
+import closeImg from '../../assets/close.svg';
+
+interface NewTransactionModalProps {
+  isOpen: boolean;
+  onRequestClose: () => void;
+
+}
+
+export function NewTransactionModal({ isOpen, onRequestClose }: NewTransactionModalProps) {
+  return (
+    <Modal
+      isOpen={isOpen}
+      onRequestClose={onRequestClose}
+      overlayClassName="react-modal-overlay"
+      className="react-modal-content"
+    >
+
+      <button
+        type="button"
+        className="react-modal-close"
+      >
+        <img src={closeImg} alt="Fechar modal" onClick={onRequestClose} />
+      </button>
+      
+      <Container>
+        <h2>Cadastrar transação</h2>
+
+        <input
+          placeholder="Título"
+        />
+
+        <input type="number"
+          placeholder="Valor"
+        />
+
+        <input
+          placeholder="Categoria"
+        />
+
+        <button type="submit">
+          Cadastrar
+        </button>
+
+
+      </Container>
+    </Modal>
+
+  )
+}
---
+++ Modulo-02/dtmoney/src/components/NewTransactionModal/styles.ts
@@ -0,0 +1,48 @@
+import styled from 'styled-components';
+
+export const Container = styled.form`
+
+  h2 {
+    color: var(--text-title);
+    font-size: 1.5rem;
+    margin-bottom: 2rem;
+
+  }
+
+  input {
+    width: 100%;
+    height: 4rem;
+    padding: 0 1.5rem;
+    border-radius: 0.25rem;
+    background: var(--input-background);
+    border: 1px solid #d7d7d7;
+    font-weight: 400;
+    font-size: 1rem;
+
+    &::placeholder {
+      color: var(--text-body);
+    }
+
+    & + input {
+      margin-top: 1rem;
+    }
+  }
+
+  button[type="submit"] {
+    width: 100%;
+    height: 4rem;
+    padding: 0 1.5rem;
+    background: var(--green);
+    color: #FFF;
+    border: 0;
+    font-size: 1rem;
+    font-weight: 600;
+    margin-top: 1.5rem;
+    transition: filter 0.2s;
+
+    &:hover {
+      filter: brightness(0.9);
+    }
+  }
+  
+`;
