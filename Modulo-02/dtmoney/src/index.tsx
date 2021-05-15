faa8e0351cd78b68ae68ed8b42cad8bd66b7a9ae
the-one-who-knoccks <thiagoalves.devp@gmail.com>
2021-05-14 22:50:28 -0300

    :alien: Updated the mirage configuration
    Updated the mirage configuration

--- Modulo-02/dtmoney/src/index.tsx
+++ Modulo-02/dtmoney/src/index.tsx
@@ -1,23 +1,48 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { createServer } from 'miragejs';
+import { createServer, Model } from 'miragejs';
 import App from './App';
 
 createServer({
+  models: {
+    transaction: Model,
+  },
+
+  seeds(server) {
+    server.db.loadData({
+      transactions: [
+        {
+          id: 1,
+          title: 'Desenvolvimento de software',
+          type: 'deposit',
+          category: 'dev',
+          amount: 6000,
+          createdAt: new Date('2021-02-12 09:00:00')
+        },
+
+        {
+          id: 2,
+          title: 'Aluguel',
+          type: 'withdraw',
+          category: 'Apt',
+          amount: 3000,
+          createdAt: new Date('2021-02-14 14:00:00')
+        }
+      ],
+    })
+  },
+
   routes() {
     this.namespace = 'api';
 
     this.get('/transactions', () => {
-      return [
-        {
-          id: 1,
-          title: 'Transaction 1',
-          amount: 400,
-          type: 'deposit',
-          category: 'Food',
-          createdAt: new Date()
-        }
-      ]
+      return this.schema.all('transaction');
+    })
+
+    this.post('/transactions', (schema, request) => {
+      const data = JSON.parse(request.requestBody)
+
+      return schema.create('transaction', data);
     })
   }
 })
